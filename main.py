d26374ca1e189018f6e1351417272d199ec8d3a1
sonah5009 <sonah5009@gmail.com>
2023-03-09 19:57:37 +0900

    Update: main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 # Weekly Assignment 2: Git Basics
 from typing import List
-class Sum:
-  def twoSum(self, nums: List[int], target: int) -> List[int]:
-    for i in range(len(nums)):
-      for j in range(i+1,len(nums)):
-        if target == nums[i]+nums[j]:
-          return [i,j]
 
-#Example 2: 
-nums = [3, 2, 4]
-target = 6
-soln1 = Sum()
-print(soln1.twoSum(nums, target))
+def twoSum(nums: List[int], target: int) -> List[int]:
+  for i in range(len(nums)):
+    for j in range(i+1,len(nums)):
+      if target == nums[i]+nums[j]:
+        return [i,j]
+
+# Output
+# nums = [3, 2, 4]
+# target = 6
+# print(twoSum(nums, target))
